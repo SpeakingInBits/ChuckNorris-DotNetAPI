4c823fdc3942e229002ea1b080d3340f9206e583
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2019-03-21 22:59:05 -0700

    Add project files.

---
+++ ChuckNorrisAPI/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ChuckNorrisAPI
+{
+    public class ChuckNorrisClient
+    {
+        private static HttpClient client;
+
+        public static void GetRandomJoke()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Joke
+        {
+            public int Id { get; set; }
+            public string JokeText { get; set; }
+            public List<string> Categories { get; set; }
+        }
+
+        public class WebResponse
+        {
+            public string Type { get; set; }
+            public Joke Value { get; set; }
+        }
+    }
+}
