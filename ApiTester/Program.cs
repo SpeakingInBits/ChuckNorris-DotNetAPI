74b40ac614125625fd3841b9d34e8619d8958f8d
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2019-03-25 22:41:58 -0700

    Add test code for categories and specific joke

--- ApiTester/Program.cs
+++ ApiTester/Program.cs
@@ -2,6 +2,7 @@
 using static System.Console;
 using ChuckNorrisAPI;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ApiTester
 {
@@ -9,8 +10,25 @@ namespace ApiTester
     {
         static async Task Main()
         {
+            //get random joke test
+            WriteLine("Getting a single joke**********************");
             Joke j = await ChuckNorrisClient.GetRandomJoke();
             WriteLine($"{j.Id}:{j.JokeText}\n\n{string.Join(", ", j.Categories)}");
+
+            //get categories
+            WriteLine("Getting categories****************************");
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            foreach (var cat in categories)
+            {
+                WriteLine(cat);
+            }
+
+            //print specific joke
+            WriteLine("Getting specific joke***************************");
+            Joke j2 = await ChuckNorrisClient.GetJokeById(2);
+            WriteLine(j2.JokeText);
+
+
             ReadKey();
         }
     }
