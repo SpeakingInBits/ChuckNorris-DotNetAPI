7ec3d7e9fd9e027cd93a6a3ffbe96aa5b0331199
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2019-03-25 23:05:44 -0700

    Refactor classes into separate files

---
+++ ChuckNorrisAPI/APIResponseClasses.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckNorrisAPI
+{
+    internal class GeneralResponse
+    {
+        public string Type { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    internal class SingleJokeResponse
+    {
+        public string Type { get; set; }
+
+        [JsonProperty("value")]
+        public Joke JokeData { get; set; }
+    }
+
+    internal class MultiJokeResponse
+    {
+        public string Type { get; set; }
+
+        [JsonProperty("value")]
+        public IEnumerable<Joke> Jokes { get; set; }
+    }
+
+    internal class CategoriesResponse
+    {
+        public string Type { get; set; }
+
+        [JsonProperty("value")]
+        public string[] Categories { get; set; }
+    }
+}
---
+++ ChuckNorrisAPI/Joke.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckNorrisAPI
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        [JsonProperty("joke")]
+        public string JokeText { get; set; }
+
+        public IEnumerable<string> Categories { get; set; }
+    }
+}
