3078c1fea25f1121d703042fea6671f078847ea6
Steven Ballard <stevenballardstudios@gmail.com>
2019-10-15 14:40:59 -0700

    Replaced .net core win form with a .net framework win form

---
+++ GetJokesForm/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace GetJokesForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetJoke = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetJoke
+            // 
+            this.btnGetJoke.Location = new System.Drawing.Point(51, 80);
+            this.btnGetJoke.Name = "btnGetJoke";
+            this.btnGetJoke.Size = new System.Drawing.Size(122, 43);
+            this.btnGetJoke.TabIndex = 0;
+            this.btnGetJoke.Text = "Get Random Joke";
+            this.btnGetJoke.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetJoke);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetJoke;
+    }
+}
+
