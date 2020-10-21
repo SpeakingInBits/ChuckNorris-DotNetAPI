0e73888f17856d6544c1765b3bb350fba5f4dc07
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-20 21:24:19 -0700

    Downloaded JSON package

--- ChuckNorrisJokesForms/Form1.Designer.cs
+++ ChuckNorrisJokesForms/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.generateJokeBtn = new System.Windows.Forms.Button();
+            this.categoryCbox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // generateJokeBtn
+            // 
+            this.generateJokeBtn.Location = new System.Drawing.Point(35, 65);
+            this.generateJokeBtn.Name = "generateJokeBtn";
+            this.generateJokeBtn.Size = new System.Drawing.Size(135, 80);
+            this.generateJokeBtn.TabIndex = 0;
+            this.generateJokeBtn.Text = "Generate Joke";
+            this.generateJokeBtn.UseVisualStyleBackColor = true;
+            // 
+            // categoryCbox
+            // 
+            this.categoryCbox.FormattingEnabled = true;
+            this.categoryCbox.Location = new System.Drawing.Point(226, 65);
+            this.categoryCbox.Name = "categoryCbox";
+            this.categoryCbox.Size = new System.Drawing.Size(121, 21);
+            this.categoryCbox.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 335);
+            this.ClientSize = new System.Drawing.Size(386, 226);
+            this.Controls.Add(this.categoryCbox);
+            this.Controls.Add(this.generateJokeBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +63,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button generateJokeBtn;
+        private System.Windows.Forms.ComboBox categoryCbox;
     }
 }
 
